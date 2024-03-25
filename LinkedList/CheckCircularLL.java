d66d09b5b0f01766997d274bd09e9273c1447716
Shivangi Singh <122040215+competitiveblood@users.noreply.github.com>
2024-03-25 22:19:31 +0530

    Create CheckCircularLL.java

---
+++ LinkedList/CheckCircularLL.java
@@ -0,0 +1,16 @@
+class GfG {
+    boolean isCircular(Node head) {
+        if (head == null) {
+            return false;
+        }
+
+        Node temp = head;
+        while (temp != null) {
+            temp = temp.next;
+            if (temp == head) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
