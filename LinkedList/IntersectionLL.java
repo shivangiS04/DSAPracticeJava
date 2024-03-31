dcc447cefc75b7f53ab56d3ccef92a1c8a3d0901
Shivangi Singh <122040215+competitiveblood@users.noreply.github.com>
2024-03-31 21:08:01 +0530

    Update IntersectionLL.java

--- LinkedList/IntersectionLL.java
+++ LinkedList/IntersectionLL.java
@@ -1,1 +1,26 @@
-
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+       ArrayList<ListNode> arr1 = new ArrayList<>();
+       ArrayList<ListNode> arr2 = new ArrayList<>();
+       while(headA!=null){
+           arr1.add(headA);
+           headA = headA.next;
+       }
+       while(headB!=null){
+           arr2.add(headB);
+           headB= headB.next;
+       }
+       for(int i = 0;i<Math.min(arr1.size(),arr2.size());i++){
+           if(arr1.size() > arr2.size()){
+               if(arr1.contains(arr2.get(i))){
+                   return arr2.get(i);
+                }
+           }
+           else{
+               if(arr2.contains(arr1.get(i))){
+                   return arr1.get(i);
+                }
+           }
+       }
+       return null;
+    }
