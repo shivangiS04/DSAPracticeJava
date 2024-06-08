0a633f63c5be730dd18504c435332060babe5ef6
Shivangi Singh <122040215+competitiveblood@users.noreply.github.com>
2024-06-08 21:40:09 +0530

    Update 796.RotateString.java

--- Strings/796.RotateString.java
+++ Strings/796.RotateString.java
@@ -1,7 +1,13 @@
+//time complexity-O(n)
+//space complexity-O(n)
 class Solution {
     public boolean rotateString(String s, String goal) {
-
-        return(s.length()==goal.length()&& (s+s).contains(goal));
-        
+        // Check if the lengths of s and goal are equal.
+        // If they are not equal, s cannot be rotated to become goal.
+        // If they are equal, proceed to the next check.
+        return (s.length() == goal.length() && 
+                // Concatenate s with itself and check if the resulting string contains goal.
+                // If goal is a rotation of s, it must appear as a substring within s+s.
+                (s + s).contains(goal));
     }
 }
