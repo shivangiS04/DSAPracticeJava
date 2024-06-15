1b777e631ebaf5bc7a7bf7147f9216c9d6d3f235
Shivangi Singh <122040215+competitiveblood@users.noreply.github.com>
2024-06-15 20:11:47 +0530

    Create 876.MiddleOfLL.java

---
+++ LinkedList/876.MiddleOfLL.java
@@ -0,0 +1,15 @@
+
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        ListNode slowPtr = head;
+        ListNode fastPtr = head;
+
+        while (fastPtr != null && fastPtr.next!= null){
+            slowPtr = slowPtr.next;
+
+            fastPtr = fastPtr.next.next;
+        }
+        return slowPtr;
+        
+    }
+}
