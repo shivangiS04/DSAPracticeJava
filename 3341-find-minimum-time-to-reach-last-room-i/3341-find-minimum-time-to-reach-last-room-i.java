aac2fb225e7e4d617007bad5152cf25a50332a1a
Shivangi Singh <122040215+shivangiS04@users.noreply.github.com>
2025-05-08 01:45:40 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 3341-find-minimum-time-to-reach-last-room-i/3341-find-minimum-time-to-reach-last-room-i.java
@@ -0,0 +1,45 @@
+public class Solution {
+    public int minTimeToReach(int[][] moveTime) {
+        int n = moveTime.length;
+        int m = moveTime[0].length;
+        boolean[][] visited = new boolean[n][m];
+        
+        int[][] bestTime = new int[n][m];
+        for (int[] row : bestTime) {
+            Arrays.fill(row, Integer.MAX_VALUE);
+        }
+        
+        int[][] dir = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+        int ans = Integer.MAX_VALUE;
+
+        // Helper function
+        void helper(int i, int j, int time) {
+            if (time >= bestTime[i][j]) return;
+            bestTime[i][j] = time;
+
+            if (i == n - 1 && j == m - 1) {
+                ans = Math.min(ans, time);
+                return;
+            }
+
+            for (int[] d : dir) {
+                int x = i + d[0];
+                int y = j + d[1];
+                if (x >= 0 && x < n && y >= 0 && y < m && !visited[x][y]) {
+                    visited[x][y] = true;
+                    int wait = moveTime[x][y];
+                    int nextTime = wait > time ? wait + 1 : time + 1;
+                    if (i == 0 && j == 0 && wait <= time) {
+                        nextTime = 1 + wait;
+                    }
+                    helper(x, y, nextTime);
+                    visited[x][y] = false;
+                }
+            }
+        }
+
+        visited[0][0] = true;
+        helper(0, 0, 0);
+        return ans;
+    }
+}
