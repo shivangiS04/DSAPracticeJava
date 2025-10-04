6d5d7988eab03424bc96d650e20c032c46d67df8
Shivangi Singh <122040215+shivangiS04@users.noreply.github.com>
2025-10-05 02:56:45 +0530

    Time: 5 ms (77.22%), Space: 57.6 MB (81.88%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -1,24 +1,20 @@
 class Solution {
     public int maxArea(int[] height) {
-        if (height == null || height.length < 2) {
-            return 0;
-        }
-        int max = 0;
         int left = 0;
         int right = height.length - 1;
+        int maxArea = 0;
 
         while (left < right) {
-            // Calculate the area and update max
-            max = Math.max(max, Math.min(height[left], height[right]) * (right - left));
+            int currentArea = Math.min(height[left], height[right]) * (right - left);
+            maxArea = Math.max(maxArea, currentArea);
 
-            // Move the pointer pointing to the shorter line inward
-            if (height[left] <= height[right]) {
+            if (height[left] < height[right]) {
                 left++;
             } else {
                 right--;
             }
         }
-        
-        return max; // Return the maximum area
+
+        return maxArea;
     }
 }
