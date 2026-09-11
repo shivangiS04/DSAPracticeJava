830f6e07f4fda4ed7b592936b47c3ff7541d10f0
Shivangi Singh <122040215+shivangiS04@users.noreply.github.com>
2026-09-11 06:26:49 +0530

    Time: 9 ms (26.4%), Space: 46.9 MB (11.87%) - LeetHub

--- 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.java
+++ 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.java
@@ -11,21 +11,23 @@ class Solution {
         for (int a = 1; a <= 9; a++) {
             for (int b = 0; b <= 9; b++) {
                 for (int c = 0; c <= 8; c += 2) {
-                    if (freq[a] == 0 || freq[b] == 0 || freq[c] == 0) {
-                        continue;
+                    int[] need = new int[10];
+                    need[a]++;
+                    need[b]++;
+                    need[c]++;
+
+                    boolean possible = true;
+
+                    for (int d = 0; d <= 9; d++) {
+                        if (need[d] > freq[d]) {
+                            possible = false;
+                            break;
+                        }
                     }
 
-                    freq[a]--;
-                    freq[b]--;
-                    freq[c]--;
-
-                    if (freq[a] >= 0 && freq[b] >= 0 && freq[c] >= 0) {
+                    if (possible) {
                         count++;
                     }
-
-                    freq[a]++;
-                    freq[b]++;
-                    freq[c]++;
                 }
             }
         }
